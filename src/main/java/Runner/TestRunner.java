3e5b9d1163059069ede05ff970736eb98353401a
Mayur Humbal <mayurhumbal2002@gmail.com>
2019-11-07 12:34:22 +0530

    mayur

---
+++ src/main/java/Runner/TestRunner.java
@@ -0,0 +1,17 @@
+package Runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "E:\\Cucumber\\FreeCrmBDDFramework\\src\\main\\java\\Features\\login.feature"
+		,glue={"stepDefinitions"}
+		,format = {"pretty","html:report.html"}
+		)
+
+public class TestRunner {
+
+}
